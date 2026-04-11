e4b6bd81929c7a5037e2a3bc598e94d3137d0e35
Costta-007 <gustavo.costtaalves@gmail.com>
2026-04-11 17:53:34 -0300

    Add files via upload

---
+++ aaa/Try e except.py
@@ -0,0 +1,28 @@
+# try:
+#     a = int(input('>>>'))
+#     b = int(input('>>>'))
+#     lista  =  [a,b]
+#     print(a/b)
+#     i  =  int(input('Digite um indice da lista: '))
+#     print(lista[i])
+# except ZeroDivisionError as erro:
+#     print(erro)
+# except ValueError as erro :
+#     print(erro)
+# except TypeError as erro:
+#     print(erro)
+# except IndexError as erro:
+#     print(erro)
+# else:
+#     print('Erro não identificado') 
+# finally:
+#     print('Fim de carregamento ...')           
+
+
+
+
+ # Trate o TypeError - Qual expressão deve ser gerada para ele aparecer?
+
+entrada = int(input("SEu nome"))
+print(entrada)
+ # Trate o ValueError - Crie em python o erro
